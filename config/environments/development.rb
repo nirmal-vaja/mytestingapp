a606561360ae34cc0d0349c69b8150e692301ce0
Nirmal Vaja <nirmalvaja123@gmail.com>
2023-01-06 19:34:16 +0530

    Fixing aws error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts << "ec2-3-109-201-73.ap-south-1.compute.amazonaws.com"
+  config.hosts << /.*\amazonaws\.com$/
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
